bf49c03a01ad6a3b86e46e9bd03e417d0e58d2ab
LucasZielinski <77624034+LucasZielinski@users.noreply.github.com>
2022-04-25 22:54:55 +0200

    Aménagement

--- Agence immobilière/VIZImmo/appli/tests/testTailleMax.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once '../modeles/mesFonctionsAccesAuxDonnees.php';
-$nom_table="utilisateur";
-$nom_colonne="login";
-echo "Test login utilisateurs";
-var_dump(recupTailleMax(connexionBDD(),$nom_table,$nom_colonne));
