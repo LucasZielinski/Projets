66ea1d02738b3bcac9b2724a00ffaac0ae198a0e
LucasZielinski <77624034+LucasZielinski@users.noreply.github.com>
2022-04-25 22:55:32 +0200

    Aménagement

--- Agence immobilière/VIZImmo/appli/tests/testajouterBien.php
+++
@@ -1,9 +0,0 @@
-<?php
-//on insère le fichier qui contient les fonctions
-include_once '../modeles/mesFonctionsAccesAuxDonnees.php';
-
-//appel de la fonction qui permet de se connecter à la base de données
-$lePdo = connexionBDD();
-
-//var_dump permet d'afficher le contenu d'un objet. Utilisable uniquement lors de test de validation
-var_dump(ajouterBien($lePdo,'L4','58 rue Hoche','59115','Leers',275000,3));
